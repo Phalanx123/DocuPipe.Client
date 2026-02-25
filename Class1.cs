6778cb5436751096d3fad693a4987cff7aac80dd
AndreFerreira <aferreira@pfwork.com.au>
2026-02-26 07:33:33 +1100

    Remove tracked build artifacts

--- Class1.cs
+++
@@ -1,4 +0,0 @@
-namespace DocuPipe.Client;
-
-// Intentionally left as a compatibility shim for initial package scaffolding.
-internal static class PackageMarker;
--- ServiceCollectionExtensions.cs
+++ ServiceCollectionExtensions.cs
@@ -10,43 +10,37 @@ namespace DocuPipe.Client;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddDocuPipeClient(this IServiceCollection services, IConfiguration configuration, string sectionName = "DocuPipe")
+    extension(IServiceCollection services)
     {
-        return services.AddDocuPipeClient(builder => builder.Bind(configuration.GetSection(sectionName)));
-    }
+        public IServiceCollection AddDocuPipeClient(IConfiguration configuration, string sectionName = "DocuPipe")
+        {
+            return services.AddDocuPipeClient(builder => builder.Bind(configuration.GetSection(sectionName)));
+        }
 
-    public static IServiceCollection AddDocuPipeClient(this IServiceCollection services, Action<DocuPipeClientOptions> configure)
-    {
-        services.Configure(configure);
-        RegisterClients(services);
-        return services;
-    }
+        public IServiceCollection AddDocuPipeClient(Action<DocuPipeClientOptions> configure)
+        {
+            services.Configure(configure);
+            RegisterClients(services);
+            return services;
+        }
 
-    public static IServiceCollection AddDocuPipeClient(this IServiceCollection services, Action<OptionsBuilder<DocuPipeClientOptions>> configure)
-    {
-        var optionsBuilder = services.AddOptions<DocuPipeClientOptions>();
-        configure(optionsBuilder);
+        public IServiceCollection AddDocuPipeClient(Action<OptionsBuilder<DocuPipeClientOptions>> configure)
+        {
+            var optionsBuilder = services.AddOptions<DocuPipeClientOptions>();
+            configure(optionsBuilder);
 
-        RegisterClients(services);
-        return services;
+            RegisterClients(services);
+            return services;
+        }
     }
 
     private static void RegisterClients(IServiceCollection services)
     {
-        services.AddHttpClient<IStandardizationClient, StandardizationClient>((serviceProvider, httpClient) =>
-        {
-            ApplyOptions(serviceProvider, httpClient);
-        });
+        services.AddHttpClient<IStandardizationClient, StandardizationClient>(ApplyOptions);
 
-        services.AddHttpClient<IDocumentClient, DocumentClient>((serviceProvider, httpClient) =>
-        {
-            ApplyOptions(serviceProvider, httpClient);
-        });
+        services.AddHttpClient<IDocumentClient, DocumentClient>(ApplyOptions);
 
-        services.AddHttpClient<ISchemaClient, SchemaClient>((serviceProvider, httpClient) =>
-        {
-            ApplyOptions(serviceProvider, httpClient);
-        });
+        services.AddHttpClient<ISchemaClient, SchemaClient>(ApplyOptions);
     }
 
     private static void ApplyOptions(IServiceProvider serviceProvider, HttpClient httpClient)
@@ -58,11 +52,13 @@ public static class ServiceCollectionExtensions
             httpClient.BaseAddress = new Uri(options.BaseUrl);
         }
 
-        if (!string.IsNullOrWhiteSpace(options.ApiKey))
+        if (string.IsNullOrWhiteSpace(options.ApiKey))
         {
-            var headerName = string.IsNullOrWhiteSpace(options.ApiKeyHeaderName) ? "x-api-key" : options.ApiKeyHeaderName;
-            httpClient.DefaultRequestHeaders.Remove(headerName);
-            httpClient.DefaultRequestHeaders.Add(headerName, options.ApiKey);
+            return;
         }
+        
+        var headerName = string.IsNullOrWhiteSpace(options.ApiKeyHeaderName) ? "x-api-key" : options.ApiKeyHeaderName;
+        httpClient.DefaultRequestHeaders.Remove(headerName);
+        httpClient.DefaultRequestHeaders.Add(headerName, options.ApiKey);
     }
 }
